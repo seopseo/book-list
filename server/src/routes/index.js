b77e7c5832d2a0d6f6d228fb8fc13ba9d4723299
seopseo <jk97104@naver.com>
2021-10-26 13:03:24 +0900

    implemented server setting

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const book = require('./book')
+// const book = require('./book')
+const word = require('./word')
 
-router.use('/books', book)
+// router.use('/books', book)
+router.use('/words', word)   // /api/word로 실행
 
 module.exports = router
